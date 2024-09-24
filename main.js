9e01918d5985dccccd644c4e4c4b97181039a600
Vkzapple <evellykhnz@gmail.com>
2024-09-24 20:48:33 +0700

    final commit

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
       const header = document.querySelector('header');
       header.classList.toggle('scrolled', window.scrollY > 0);
     });
+
+    //logo teks bisa ngerefresh
+    function refreshPage() {
+      location.reload();
+    }
+
+    document.getElementById("logo").addEventListener("click", refreshPage);
